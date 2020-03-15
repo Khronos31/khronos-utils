0a514872fb9ef81e618d17b1003f6260a1596c4e
Khronos31 <khronos3141@gmail.com>
2020-03-15 20:51:24 +0900

    update khronosutils.sh

--- khronosutils.sh
+++ khronosutils.sh
@@ -1,3 +1,10 @@
 yy(){
-  clip "$(history 2|sed '1s/^  [0-9]*  //;$d')"
+  if fc -ld >/dev/null 2>&1; then
+    fc -e '(){cat "$1"&&echo>"$1"}' -1 2>/dev/null
+  else
+    fc -ln -1
+  fi |
+  sed -e '1s/^\s*//;:a;N;$!ba;s/\s*$//' |
+  head -c -1 |
+  clip
 }
