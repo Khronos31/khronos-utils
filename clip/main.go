c3a2f91379d6e82dd16d69672f777dbfd800ccbe
Khronos31 <khronos3141@gmail.com>
2020-03-17 15:10:23 +0900

    update

--- clip/main.go
+++ clip/main.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"io/ioutil"
-	"os"
-)
-
-//#cgo LDFLAGS: -framework Foundation -framework UIKit
-//#cgo CFLAGS: -ObjC
 /*
+#cgo LDFLAGS: -framework Foundation -framework UIKit -isysroot /usr/share/SDKs/iPhoneOS.sdk
+#cgo CFLAGS: -ObjC -isysroot /usr/share/SDKs/iPhoneOS.sdk
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
@@ -19,16 +15,23 @@ void clipText(char *text) {
 */
 import "C"
 
+import (
+	"io/ioutil"
+	"os"
+)
+
 func main() {
+	var text string
 	if len(os.Args) >= 2 {
 		var data = make([]byte, 0, 100*1000)
 		for i := 1; i < len(os.Args); i++ {
 			data = append(data, ' ')
 			data = append(data, os.Args[i]...)
 		}
-		C.clipText(C.CString(string(data[1:])))
+		text = string(data[1:])
 	} else {
 		data, _ := ioutil.ReadAll(os.Stdin)
-		C.clipText(C.CString(string(data)))
+		text = string(data)
 	}
+	C.clipText(C.CString(text))
 }
---
+++ say/main.go
@@ -0,0 +1,40 @@
+package main
+
+/*
+#cgo LDFLAGS: -framework Foundation -framework AVFoundation -isysroot /usr/share/SDKs/iPhoneOS.sdk
+#cgo CFLAGS: -ObjC -isysroot /usr/share/SDKs/iPhoneOS.sdk
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+void speechText(char *text);
+
+void speechText(char *text) {
+	AVSpeechSynthesizer *speechSynthesizer = [[AVSpeechSynthesizer alloc] init];
+	NSString *speakingText = [NSString stringWithCString:text encoding:NSUTF8StringEncoding];
+	AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:speakingText];
+	[speechSynthesizer speakUtterance:utterance];
+}
+*/
+import "C"
+
+import (
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	var text string
+	if len(os.Args) >= 2 {
+		var data = make([]byte, 0, 100*1000)
+		for i := 1; i < len(os.Args); i++ {
+			data = append(data, ' ')
+			data = append(data, os.Args[i]...)
+		}
+		text = string(data[1:])
+	} else {
+		data, _ := ioutil.ReadAll(os.Stdin)
+		text = string(data)
+	}
+	C.speechText(C.CString(text))
+}
