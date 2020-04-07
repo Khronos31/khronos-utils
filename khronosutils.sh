8503450191a730e71008396e6e54eee4dac4e94e
Khronos31 <khronos3141@gmail.com>
2020-04-07 14:53:49 +0900

    update khronosutil.sh

--- khronosutils.sh
+++ khronosutils.sh
@@ -1,10 +1,10 @@
-yy(){
-  if fc -ld >/dev/null 2>&1; then
-    fc -e '(){cat "$1"&&echo>"$1"}' -1 2>/dev/null
+yy() {
+  if fc -ld > /dev/null 2>&1; then
+    fc -e '(){cat "$1"&&echo>"$1"}' -1 2> /dev/null
   else
     fc -ln -1
   fi |
-  sed -e '1s/^\s*//;:a;N;$!ba;s/\s*$//' |
-  head -c -1 |
-  clip
+    sed -e '1s/^\s*//;:a;N;$!ba;s/\s*$//' |
+    head -c -1 |
+    clip
 }
